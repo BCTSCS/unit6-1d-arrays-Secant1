49c93ed98ab0d2b7f108414aa0689473d8ea1010
Secant1 <rbclews@gmail.com>
2025-02-25 14:13:22 -0500

    asdfsd

--- DataAnalyzer.java
+++ DataAnalyzer.java
@@ -1,73 +1,110 @@
-import java.io.File;
-import java.util.Scanner;
+import FileOperator.FileOperator;
 
 public class DataAnalyzer {
 
-    public static int binaryList(int[] numbers, int target) {
-        int minIndex = 0; // Adjusted for 0-based indexing
-        int maxIndex = numbers.length - 1;
+    String[] countries;
+    int[] populations;
+    double[] unemployment;
+    String[] incomes;
 
-        while (minIndex <= maxIndex) {
-            int middleIndex = (minIndex + maxIndex) / 2;
-
-            if (target == numbers[middleIndex]) {
-                return middleIndex;
-            } else if (target > numbers[middleIndex]) {
-                minIndex = middleIndex + 1;
-            } else {
-                maxIndex = middleIndex - 1;
-            }
-        }
-
-        return -1; // Target not found
+    DataAnalyzer(String path) {
+        countries = FileOperator.toStringArray(path + "countries.txt", 215);
+        populations = FileOperator.toIntArray(path + "populations.txt", 215);
+        unemployment = FileOperator.toDoubleArray(path + "unemployment.txt", 215);
+        incomes = FileOperator.toStringArray(path + "incomes.txt", 215);
     }
 
-    // Linear Search
-    public static int searchList(int[] numbers, int target) {
-        int index = 0;
-
-        while (index <= numbers.length - 1) {
-            if (numbers[index] == target) {
-                return index;
+    String getCountryByPopulation(int pop) {
+        return countries[FileOperator.getIntIndex(populations, pop)];
+    }
+    String getCountryByUnemployment(double unemp) {
+        return countries[FileOperator.getDoubleIndex(unemployment, unemp)];
+    }
+    String[] getCountriesByIncome(String income) {
+        String[] tlist = new String[215];
+        int j = 0;
+        for (int i = 0; i < 215; i++) {
+            if (incomes[i] == income) {
+                tlist[j] = countries[i];
+                j++;
             }
-            index++;
         }
-        return -1;
+
+        String[] rlist = new String[j];
+        for (int i = 0; i < j; i++) {
+            rlist[i] = tlist[i];
+        }
+        return rlist;
     }
 
-    // Reverse Linear Search
-    public static int reverseList(int[] numbers, int target) {
-        for (int i = numbers.length - 1; i >= 0; i--) {
-            if (numbers[i] == target) {
-                return i;
+
+    double getIncomeLevelPercent(String income_level) {
+        int i = 0;
+        for (String income : incomes) {
+            if (income.equals(income_level)) {
+                i++;
             }
         }
-        return -1;
+        return (double)i/255;
+    }
+
+    String[] getUnemploymentInterval(double low, double high) {
+        String[] tlist = new String[215];
+        int j = 0;
+        for (int i = 0; i < 215; i++) {
+            if (unemployment[i] >= low && unemployment[i] <= high) {
+                tlist[j] = countries[i];
+                j++;
+            }
+        }
+
+        String[] rlist = new String[j];
+        for (int i = 0; i < j; i++) {
+            rlist[i] = tlist[i];
+        }
+        return rlist;
+    }
+
+    String[] getPopulationInterval(int low, int high) {
+        String[] tlist = new String[215];
+        int j = 0;
+        for (int i = 0; i < 215; i++) {
+            if (populations[i] >= low && populations[i] <= high) {
+                tlist[j] = countries[i];
+                j++;
+            }
+        }
+
+        String[] rlist = new String[j];
+        for (int i = 0; i < j; i++) {
+            rlist[i] = tlist[i];
+        }
+        return rlist;
     }
 
     public static void main(String[] args) {
-
-        // Scanner input;
-        int[] arr = new int[100];
-
-        try{
-            File file = new File("C:/Users/zipit/github-classroom/BCTSCS/unit6-1d-arrays-Secant1/numbers.txt");
-            Scanner input = new Scanner(file);
-
-
-
-            for (int i = 0; i< 100; i++) {
-                arr[i] = input.nextInt();
-            }
-        }catch(Exception e){}
-
-        // System.out.print("What number would you like to find: ");
-        // int number = input.nextInt();
-        // System.out.println();
-        int number = 0;
-
-        System.out.println("Forward Search (50): " + searchList(arr, number));
-        System.out.println("Binary Search (50): " + binaryList(arr, number));
-        System.out.println("Reverse Search (50): " + reverseList(arr, number));
+        DataAnalyzer analyzer = new DataAnalyzer("C:/Users/zipit/github-classroom/BCTSCS/unit6-1d-arrays-Secant1/data/");
+    
+        // Showcase method functionalities
+        System.out.println("Country with population 331002651: " + analyzer.getCountryByPopulation(331002651));
+        System.out.println("Country with 5.5% unemployment: " + analyzer.getCountryByUnemployment(5.5));
+    
+        System.out.println("\nCountries with 'High income':");
+        for (String country : analyzer.getCountriesByIncome("High income")) {
+            System.out.println(country);
+        }
+    
+        System.out.println("\nPercentage of 'Low income' countries: " + analyzer.getIncomeLevelPercent("Low income") * 100 + "%");
+    
+        System.out.println("\nCountries with unemployment rate between 3.0% and 7.0%:");
+        for (String country : analyzer.getUnemploymentInterval(3.0, 7.0)) {
+            System.out.println(country);
+        }
+    
+        System.out.println("\nCountries with populations between 10M and 100M:");
+        for (String country : analyzer.getPopulationInterval(10000000, 100000000)) {
+            System.out.println(country);
+        }
     }
+    
 }
--- FileOperator/FileOperator.java
+++ FileOperator/FileOperator.java
@@ -4,19 +4,13 @@ import java.io.*;
 import java.util.*;
 
 public class FileOperator {
-    private String filename;
 
-    public FileOperator(String fn) {
-        filename = fn;
-    }
-
-    public String[] toStringArray(int arraySize) {
-        String[] arr = new String[arraySize];
+    public static ArrayList<String> toStringArray(String filename) {
+        ArrayList<String> arr = new ArrayList<>();
         try {
             Scanner myReader = new Scanner(new File(filename));
-            int i = 0;
-            while (myReader.hasNextLine() && i < arraySize) {
-                arr[i++] = myReader.nextLine();
+            while (myReader.hasNextLine()) {
+                arr.add(myReader.nextLine());
             }
             myReader.close();
         } catch (FileNotFoundException e) {
@@ -25,13 +19,12 @@ public class FileOperator {
         return arr;
     }
 
-    public int[] toIntArray(int arraySize) {
-        int[] arr = new int[arraySize];
+    public static ArrayList<Integer> toIntArray(String filename) {
+        ArrayList<Integer> arr = new ArrayList<>();
         try {
             Scanner myReader = new Scanner(new File(filename));
-            int i = 0;
-            while (myReader.hasNextInt() && i < arraySize) {
-                arr[i++] = myReader.nextInt();
+            while (myReader.hasNextLine()) {
+                arr.add(myReader.nextInt());
             }
             myReader.close();
         } catch (FileNotFoundException e) {
@@ -40,13 +33,12 @@ public class FileOperator {
         return arr;
     }
 
-    public double[] toDoubleArray(int arraySize) {
-        double[] arr = new double[arraySize];
+    public static ArrayList<Double> toDoubleArray(String filename) {
+        ArrayList<Double> arr = new ArrayList<>();
         try {
             Scanner myReader = new Scanner(new File(filename));
-            int i = 0;
-            while (myReader.hasNextDouble() && i < arraySize) {
-                arr[i++] = myReader.nextDouble();
+            while (myReader.hasNextLine()) {
+                arr.add(myReader.nextDouble());
             }
             myReader.close();
         } catch (FileNotFoundException e) {
@@ -55,132 +47,133 @@ public class FileOperator {
         return arr;
     }
 
-    public static String[][] filterData(String[][] data, int columnIndex, String keyword) {
-        int count = 0;
-        for (String[] row : data) {
-            if (row[columnIndex].contains(keyword)) {
-                count++;
+    public static int getStringIndex(String[] arr, String value) {
+        int index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(value)) {
+                index = i;
+                break;
             }
         }
-        
-        String[][] filteredData = new String[count][];
-        int index = 0;
-        for (String[] row : data) {
-            if (row[columnIndex].contains(keyword)) {
-                filteredData[index++] = row;
+        return index;
+    }
+    public static int getDoubleIndex(double[] arr, double value) {
+        int index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
+                index = i;
+                break;
             }
         }
-        return filteredData;
+        return index;
     }
-
-    public static double findMin(String[][] data, int columnIndex) {
-        double min = Double.MAX_VALUE;
-        for (String[] row : data) {
-            min = Math.min(min, Double.parseDouble(row[columnIndex]));
-        }
-        return min;
-    }
-
-    public static double findMax(String[][] data, int columnIndex) {
-        double max = Double.MIN_VALUE;
-        for (String[] row : data) {
-            max = Math.max(max, Double.parseDouble(row[columnIndex]));
-        }
-        return max;
-    }
-
-    public static double calculateAverage(String[][] data, int columnIndex) {
-        double sum = 0;
-        int count = 0;
-        for (String[] row : data) {
-            sum += Double.parseDouble(row[columnIndex]);
-            count++;
-        }
-        return count > 0 ? sum / count : 0;
-    }
-
-    public static void bubbleSort(String[][] data, int columnIndex, boolean ascending) {
-        int n = data.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                double val1 = Double.parseDouble(data[j][columnIndex]);
-                double val2 = Double.parseDouble(data[j + 1][columnIndex]);
-                if ((ascending && val1 > val2) || (!ascending && val1 < val2)) {
-                    String[] temp = data[j];
-                    data[j] = data[j + 1];
-                    data[j + 1] = temp;
-                }
+    public static int getIntIndex(int[] arr, int value) {
+        int index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
+                index = i;
+                break;
             }
         }
+        return index;
     }
+    
+    
 
-    public static int[] greaterThan(int[] data, int limit) {
-        int[] arr = new int[data.length];
-        int j = 0;
-        for (int i = 0; i < data.length - 1; i++) {
-            if (data[i] >= limit) {
-                arr[i] = data[i];
-                j++;
-            } else {
-                arr[i] = -1;
-            }
-        }
-        int[] out = new int[j];
-        j = 0;
-        for (int i = 0; i < data.length - 1; i++) {
-            if (arr[i] != -1) {
-                out[j] = arr[i];
-                j++;
-            }
-        }
-        return out;
-    }
+    // public static void main(ArrayList<String> args) {
+    //     // Initialize FileOperator with the provided file
+    //     FileOperator fileOp = new FileOperator("FileOperator/array.txt");
+    //     int arraySize = 14;
 
-    public static void main(String[] args) {
-        // Initialize FileOperator with the provided file
-        FileOperator fileOp = new FileOperator("FileOperator/array.txt");
-        int arraySize = 14;
+    //     // Test toStringArray
+    //     ArrayList<String> stringArray = fileOp.toStringArray(arraySize);
+    //     System.out.println("String Array:");
+    //     System.out.println(Arrays.toString(stringArray));
 
-        // Test toStringArray
-        String[] stringArray = fileOp.toStringArray(arraySize);
-        System.out.println("String Array:");
-        System.out.println(Arrays.toString(stringArray));
+    //     // Test toIntArray
+    //     int[] intArray = fileOp.toIntArray(arraySize);
+    //     System.out.println("Integer Array:");
+    //     System.out.println(Arrays.toString(intArray));
 
-        // Test toIntArray
-        int[] intArray = fileOp.toIntArray(arraySize);
-        System.out.println("Integer Array:");
-        System.out.println(Arrays.toString(intArray));
+    //     // Test toDoubleArray
+    //     double[] doubleArray = fileOp.toDoubleArray(arraySize);
+    //     System.out.println("Double Array:");
+    //     System.out.println(Arrays.toString(doubleArray));
 
-        // Test toDoubleArray
-        double[] doubleArray = fileOp.toDoubleArray(arraySize);
-        System.out.println("Double Array:");
-        System.out.println(Arrays.toString(doubleArray));
+    //     // Convert integer array to a 2D String array for other operations
+    //     ArrayList<String>[] data = new ArrayList<String>arraySize][1];
+    //     for (int i = 0; i < arraySize; i++) {
+    //         data[i][0] = String.valueOf(intArray[i]);
+    //     }
 
-        // Convert integer array to a 2D String array for other operations
-        String[][] data = new String[arraySize][1];
-        for (int i = 0; i < arraySize; i++) {
-            data[i][0] = String.valueOf(intArray[i]);
-        }
+    //     System.out.println("Minimum value: " + FileOperator.findMin(data, 0));
+    //     System.out.println("Maximum value: " + FileOperator.findMax(data, 0));
+    //     System.out.println("Average value: " + FileOperator.calculateAverage(data, 0));
 
-        System.out.println("Minimum value: " + FileOperator.findMin(data, 0));
-        System.out.println("Maximum value: " + FileOperator.findMax(data, 0));
-        System.out.println("Average value: " + FileOperator.calculateAverage(data, 0));
+    //     FileOperator.bubbleSort(data, 0, true);
+    //     System.out.println("Sorted (Ascending):");
+    //     for (ArrayList<String> row : data) {
+    //         System.out.println(row[0]);
+    //     }
 
-        FileOperator.bubbleSort(data, 0, true);
-        System.out.println("Sorted (Ascending):");
-        for (String[] row : data) {
-            System.out.println(row[0]);
-        }
+    //     FileOperator.bubbleSort(data, 0, false);
+    //     System.out.println("Sorted (Descending):");
+    //     for (ArrayList<String> row : data) {
+    //         System.out.println(row[0]);
+    //     }
 
-        FileOperator.bubbleSort(data, 0, false);
-        System.out.println("Sorted (Descending):");
-        for (String[] row : data) {
-            System.out.println(row[0]);
-        }
-
-        System.out.println("Greater Than 5:");
-        for (int y : FileOperator.greaterThan(fileOp.toIntArray(arraySize), 5)) {
-            System.out.println(y);
-        }
-    }
+    //     System.out.println("Greater Than 5:");
+    //     for (int y : FileOperator.greaterThan(fileOp.toIntArray(arraySize), 5)) {
+    //         System.out.println(y);
+    //     }
+    // }
 }
+
+
+
+
+
+// public static String[] toStringArray(String filename, int arraySize) {
+//     String[] arr = new String[arraySize];
+//     try {
+//         Scanner myReader = new Scanner(new File(filename));
+//         int i = 0;
+//         while (myReader.hasNextLine() && i < arraySize) {
+//             arr[i++] = myReader.nextLine();
+//         }
+//         myReader.close();
+//     } catch (FileNotFoundException e) {
+//         System.out.println("File not found.");
+//     }
+//     return arr;
+// }
+
+// public static int[] toIntArray(String filename, int arraySize) {
+//     int[] arr = new int[arraySize];
+//     try {
+//         Scanner myReader = new Scanner(new File(filename));
+//         int i = 0;
+//         while (myReader.hasNextInt() && i < arraySize) {
+//             arr[i++] = myReader.nextInt();
+//         }
+//         myReader.close();
+//     } catch (FileNotFoundException e) {
+//         System.out.println("File not found.");
+//     }
+//     return arr;
+// }
+
+// public static double[] toDoubleArray(String filename, int arraySize) {
+//     double[] arr = new double[arraySize];
+//     try {
+//         Scanner myReader = new Scanner(new File(filename));
+//         int i = 0;
+//         while (myReader.hasNextDouble() && i < arraySize) {
+//             arr[i++] = myReader.nextDouble();
+//         }
+//         myReader.close();
+//     } catch (FileNotFoundException e) {
+//         System.out.println("File not found.");
+//     }
+//     return arr;
+// }
