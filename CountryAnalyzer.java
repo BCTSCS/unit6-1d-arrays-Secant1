c8337d713d8646cb97f82f456b017a47e0766106
Secant1 <rbclews@gmail.com>
2025-03-07 23:01:13 -0500

    sdfas

--- CountryAnalyzer.java
+++ CountryAnalyzer.java
@@ -3,11 +3,16 @@ import java.util.*;
 
 public class CountryAnalyzer {
     private ArrayList<Country> countries;
+    private ArrayList<Country> filtered_countries;
 
     public CountryAnalyzer(String countriesFile, String populationsFile, String incomesFile, String unemploymentFile) {
         countries = new ArrayList<>();
+        filtered_countries = new ArrayList<>();
         loadData(countriesFile, populationsFile, incomesFile, unemploymentFile);
     }
+
+
+    // LOAD DATA
 
     private void loadData(String countriesFile, String populationsFile, String incomesFile, String unemploymentFile) {
         String[] countryNames = readStringArray(countriesFile);
@@ -21,6 +26,7 @@ public class CountryAnalyzer {
 
         for (int i = 0; i < minLength; i++) {
             countries.add(new Country(countryNames[i], incomes[i], populations[i], (float)unemploymentRates[i]));
+            filtered_countries.add(countries.get(i));
         }
     }
 
@@ -69,18 +75,283 @@ public class CountryAnalyzer {
         return list.isEmpty() ? new double[0] : list.stream().mapToDouble(d -> d).toArray();
     }
 
-    public void identifyHighRiskCommunities(String incomeCategory, double unemploymentThreshold) {
-        if (countries.isEmpty()) {
-            System.out.println("Error: Dataset is empty. Cannot analyze high-risk communities.");
-            return;
-        }
-       
-        System.out.println("High-risk communities with " + incomeCategory + " and unemployment rate above " + unemploymentThreshold + "%:");
+
+
+    // FILTER DATA
+    private void _resetFilteredCountries() {
+        filtered_countries = new ArrayList<Country>();
         for (Country country : countries) {
-            if (country.getIncomeLevel().equalsIgnoreCase(incomeCategory) && country.getUnemploymentRate() > unemploymentThreshold) {
-                System.out.println(country.getName());
+            filtered_countries.add(country);
+        }
+    }
+    private void _filterName(ArrayList<Country> data, String name_input) {
+        String[] names = name_input.split(",");
+        for (int i = 0; i < names.length; i++) {
+            names[i] = names[i].toLowerCase().trim();
+        }
+        Boolean test;
+        ArrayList<Country> remove_list = new ArrayList<>();
+
+        for (Country country : data) {
+            test = false;
+            for (String name : names) {
+                if (country.getName().toLowerCase().contains(name)) {
+                    test = true;
+                    break;
+                }
+            }
+            if (!test) {
+                remove_list.add(country);
             }
         }
+        for (Country country : remove_list) {
+            data.remove(country);
+        }
+    }
+    private void _filterIncome(ArrayList<Country> data, String income_input) {
+        String[] incomes = income_input.split(",");
+        for (int i = 0; i < incomes.length; i++) {
+            incomes[i] = incomes[i].toLowerCase().trim();
+        }
+        Boolean test;
+        ArrayList<Country> remove_list = new ArrayList<>();
+
+        for (Country country : data) {
+            test = false;
+            for (String income : incomes) {
+                if (country.getIncomeLevel().toLowerCase().equals(income)) {
+                    test = true;
+                    break;
+                }
+            }
+            if (!test) {
+                remove_list.add(country);
+            }
+        }
+        for (Country country : remove_list) {
+            data.remove(country);
+        }
+    }
+    private void _filterUnemployment(ArrayList<Country> data, double lower, double upper) {
+        ArrayList<Country> remove_list = new ArrayList<>();
+
+        for (Country country : data) {
+            if (!(country.getUnemploymentRate() > lower && country.getUnemploymentRate() < upper)) {
+                remove_list.add(country);
+            }
+        }
+        for (Country country : remove_list) {
+            data.remove(country);
+        }
+    }
+    private void _filterPopulation(ArrayList<Country> data, int lower, int upper) {
+        ArrayList<Country> remove_list = new ArrayList<>();
+
+        for (Country country : data) {
+            if (!(country.getUnemploymentRate() > lower && country.getUnemploymentRate() < upper)) {
+                remove_list.add(country);
+            }
+        }
+        for (Country country : remove_list) {
+            data.remove(country);
+        }
+    }
+    private void _filterUnemploymentRange(ArrayList<Country> data, int lower, int upper) {
+        if (upper == -1) {
+            upper = data.size();
+        }
+
+        int n = data.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (data.get(j).getUnemploymentRate() > data.get(j + 1).getUnemploymentRate()) {
+                    Country temp = data.get(j);
+                    data.set(j, data.get(j + 1));
+                    data.set(j + 1, temp);
+                }
+            }
+        }
+
+        ArrayList<Country> filtered = new ArrayList<>(data.subList(lower, upper));
+        data.clear();
+        for (Country country : filtered) {
+            data.add(country);
+        }
+    }
+    private void _filterPopulationRange(ArrayList<Country> data, int lower, int upper) {
+        if (upper == -1) {
+            upper = data.size();
+        }
+
+        int n = data.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (data.get(j).getPopulation() > data.get(j + 1).getPopulation()) {
+                    Country temp = data.get(j);
+                    data.set(j, data.get(j + 1));
+                    data.set(j + 1, temp);
+                }
+            }
+        }
+
+        ArrayList<Country> filtered = new ArrayList<>(data.subList(lower, upper));
+        data.clear();
+        for (Country country : filtered) {
+            data.add(country);
+        }
+    }
+
+    public void filterName(String name) {
+        if (!name.equals("")) {
+            _resetFilteredCountries();
+            _filterName(filtered_countries, name);
+        }
+    }
+    public void filterIncome(String income) {
+        if (!income.equals("")) {
+            _resetFilteredCountries();
+            _filterIncome(filtered_countries, income);
+        }
+    }
+    public void filterUnemployment(double lower, double upper) {
+        _resetFilteredCountries();
+        _filterUnemployment(filtered_countries, lower, upper);
+    }
+    public void filterPopulation(int lower, int upper) {
+        _resetFilteredCountries();
+        _filterPopulation(filtered_countries, lower, upper);
+    }
+    public void filterUnemploymentRange(int lower, int upper) {
+        _resetFilteredCountries();
+        _filterUnemploymentRange(filtered_countries, lower, upper);
+    }
+    public void filterPopulationRange(int lower, int upper) {
+        _resetFilteredCountries();
+        _filterPopulationRange(filtered_countries, lower, upper);
+    }
+    public void filterUnemployment(String lower, String upper) {
+        double i1, i2;
+        if (lower.equals("")) {
+            i1 = 0;
+        } else {
+            i1 = Double.parseDouble(lower);
+        }
+        if (upper.equals("")) {
+            i2 = 100.0;
+        } else {
+            i2 = Double.parseDouble(upper);
+        }
+        filterUnemployment(i1,i2);
+    }
+    public void filterPopulation(String lower, String upper) {
+        int i3, i4;
+        if (lower.equals("")) {
+            i3 = 0;
+        } else {
+            i3 = Integer.parseInt(lower);
+        }
+        if (upper.equals("")) {
+            i4 = Integer.MAX_VALUE;
+        } else {
+            i4 = Integer.parseInt(upper);
+        }
+        filterPopulation(i3, i4);
+    }
+    public void filterUnemploymentRange(String lower, String upper) {
+        int i5, i6;
+        if (lower.equals("")) {
+            i5 = 0;
+        } else {
+            i5 = Integer.parseInt(lower);
+        }
+        if (upper.equals("")) {
+            i6 = -1;
+        } else {
+            i6 = Integer.parseInt(upper);
+        }
+        filterUnemploymentRange(i5, i6);
+    }
+    public void filterPopulationRange(String lower, String upper) {
+        int i5, i6;
+        if (lower.equals("")) {
+            i5 = 0;
+        } else {
+            i5 = Integer.parseInt(lower);
+        }
+        if (upper.equals("")) {
+            i6 = -1;
+        } else {
+            i6 = Integer.parseInt(upper);
+        }
+        filterPopulationRange(i5, i6);
+    }
+
+    public void filterAll(String name, String income, double lower, double upper, int lower2, int upper2, int lower3, int upper3, int lower4, int upper4) {
+        _resetFilteredCountries();
+        if (!name.equals("")) {
+            _filterName(filtered_countries, name);
+        }
+        if (!income.equals("")) {
+            _filterIncome(filtered_countries, income);
+        }
+        _filterUnemployment(filtered_countries, lower, upper);
+        _filterPopulation(filtered_countries, lower2, upper2);
+        if (lower3 != -1 && upper3 != -1) {
+            _filterUnemploymentRange(filtered_countries, lower3, upper3);
+        }
+        if (lower4 != -1 && upper4 != -1) {
+            _filterPopulationRange(filtered_countries, lower4, upper4);
+        }
+    }
+    public void filterAll(String name, String income, String lower, String upper, String lower2, String upper2, String lower3, String upper3, String lower4, String upper4) {
+        double i1, i2;
+        int i3, i4, i5, i6, i7, i8;
+
+        if (lower.equals("")) {
+            i1 = 0;
+        } else {
+            i1 = Double.parseDouble(lower);
+        }
+        if (upper.equals("")) {
+            i2 = 100.0;
+        } else {
+            i2 = Double.parseDouble(upper);
+        }
+
+        if (lower2.equals("")) {
+            i3 = 0;
+        } else {
+            i3 = Integer.parseInt(lower2);
+        }
+        if (upper2.equals("")) {
+            i4 = Integer.MAX_VALUE;
+        } else {
+            i4 = Integer.parseInt(upper2);
+        }
+
+        if (lower3.equals("")) {
+            i5 = -1;
+        } else {
+            i5 = Integer.parseInt(lower3);
+        }
+        if (upper3.equals("")) {
+            i6 = -1;
+        } else {
+            i6 = Integer.parseInt(upper3);
+        }
+
+        if (lower4.equals("")) {
+            i7 = -1;
+        } else {
+            i7 = Integer.parseInt(lower4);
+        }
+        if (upper4.equals("")) {
+            i8 = -1;
+        } else {
+            i8 = Integer.parseInt(upper4);
+        }
+
+        filterAll(name, income, i1, i2, i3, i4, i5, i6, i7, i8);
     }
 
     public double calculateAverageUnemployment() {
@@ -95,30 +366,79 @@ public class CountryAnalyzer {
         return sum / countries.size();
     }
 
-    public String findHighestUnemploymentCountry() {
-        if (countries.isEmpty()) {
-            System.out.println("Error: No data available.");
-            return "N/A";
+
+
+    // OUTPUT DATA
+    private String clampString(String input, int length) {
+        int trail_length = length - input.length();
+        for (int i = 0; i < trail_length; i++) {
+            input = input + " ";
         }
-        
-        Country maxUnemploymentCountry = countries.get(0);
-        for (Country country : countries) {
-            if (country.getUnemploymentRate() > maxUnemploymentCountry.getUnemploymentRate()) {
-                maxUnemploymentCountry = country;
+        return input;
+    }
+
+    public String getFilteredCountryTable() {
+        int name_length = 7;
+        for (Country country : filtered_countries) {
+            if (country.getName().length() > name_length) {
+                name_length = country.getName().length();
             }
         }
-        return maxUnemploymentCountry.getName() + " with " + maxUnemploymentCountry.getUnemploymentRate() + "% unemployment";
-    }
+        String output = clampString("Country", name_length) + "    Income Level            Unemployment     Population\n";
+        for (Country country : filtered_countries) {
+            String unem = String.valueOf(Math.round(country.getUnemploymentRate()*100));
+            unem = unem.substring(0, unem.length()-2) + "." + unem.substring(unem.length()-2, unem.length()-1);
+            if (unem.substring(0,1).equals(".")) {
+                unem = "0" + unem;
+            }
+            output = output
+             + clampString(country.getName(), name_length+4)
+             + clampString(country.getIncomeLevel(), 24)
+             + clampString(unem+"%", 17)
+             + String.valueOf(country.getPopulation())
+             + "\n";
+        }
 
-    public void displayInfo() {
-        System.out.println("Total communities analyzed: " + countries.size());
+        return output;
     }
+    
+    public String getFilteredCountryStats() {
+        long sum_pop = 0;
+        long sum_unem = 0;
+        for (Country country : filtered_countries) {
+            sum_unem += country.getUnemploymentRate();
+            sum_pop += country.getPopulation();
+        }
 
-    public static void main(String[] args) {
-        CountryAnalyzer data = new CountryAnalyzer("countries.txt", "populations.txt", "incomes.txt", "unemployment.txt");
-        data.displayInfo();
-        data.identifyHighRiskCommunities("low income", 10);
-        System.out.println("Average Unemployment Rate: " + data.calculateAverageUnemployment() + "%");
-        System.out.println("Country with Highest Unemployment: " + data.findHighestUnemploymentCountry());
+        String[] income_levels = {"Low Income", "Lower Middle Income", "Upper Middle Income", "High Income"};
+        int[] income_amounts = {0,0,0,0};
+
+        for (int i = 0; i < income_levels.length; i++) {
+            for (Country country : filtered_countries) {
+                if (country.getIncomeLevel().equals(income_levels[i])) {
+                    income_amounts[i]++;
+                }
+            }
+        }
+
+        String unem = String.valueOf(Math.round(sum_unem/filtered_countries.size()*100));
+        unem = unem.substring(0, unem.length()-2) + "." + unem.substring(unem.length()-2, unem.length()-1);
+        if (unem.substring(0,1).equals(".")) {
+            unem = "0" + unem;
+        }
+
+        return "" +
+           " Total:                       " + String.valueOf(filtered_countries.size()) + "\n"
+         + " Average Unemployment Rate:   " + unem + "%\n"
+         + " Average Population:          " + String.valueOf(sum_pop/filtered_countries.size()) + "\n"
+         + "\n"
+         + " Countries per Income Level:  " + "\n"
+         + "   High:                      " + String.valueOf(income_amounts[3]) + "\n"
+         + "   Upper Middle:              " + String.valueOf(income_amounts[2]) + "\n"
+         + "   Lower Middle:              " + String.valueOf(income_amounts[1]) + "\n"
+         + "   Low:                       " + String.valueOf(income_amounts[0]) + "\n";
+         
+        
+         
     }
 }
--- DataGUI.java
+++ DataGUI.java
@@ -1,15 +1,26 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.*;
+
  
 public class DataGUI extends JFrame {
-    private JTextField inputField;
-    private JButton statusButton, percentageButton;
-    private JTextArea resultsArea;
+    private JTextField nameFilterInput, incomeFilterInput, unemploymentFilterLower, unemploymentFilterUpper, unemploymentFilterRangeLower, unemploymentFilterRangeUpper, popFilterLower, popFilterUpper, popFilterRangeLower, popFilterRangeUpper;
+    private JButton nameFilterButton, incomeFilterButton, unemploymentFilterButton, unemploymentFilterRangeButton, popFilterButton, popFilterRangeButton;
+    private JPanel nameFilterPanel, incomeFilterPanel, unemploymentFilterPanel, unemploymentFilterRangePanel, popFilterPanel, popFilterRangePanel, allFilterPanel, sidePanel, screenshotPanel;
+
+    private JButton filterAll, screenshotListButton, screenshotStatsButton;
+
+    private static int screenshot_counter = 0;
+
+    private JTextArea resultsArea, statsArea;
     private CountryAnalyzer analyzer;
  
     public DataGUI() {
         super("Data Analysis Tool");
-        setSize(500, 400);
+        setSize(1000, 800);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new FlowLayout());
  
@@ -17,38 +28,166 @@ public class DataGUI extends JFrame {
         analyzer = new CountryAnalyzer("countries.txt", "populations.txt",
                                   "incomes.txt", "unemployment.txt");
  
-        // Create components
-        inputField = new JTextField(20);
-        statusButton = new JButton("Analyze by Status");
-        percentageButton = new JButton("Status Percentage");
-        resultsArea = new JTextArea(10, 40);
+        // Create components        
+        nameFilterInput              = new JTextField(20);
+        nameFilterButton             = new JButton("    Filter by Name    ");
+        nameFilterPanel              = new JPanel(new GridLayout(1,2));
+        nameFilterPanel.add(nameFilterInput);
+        nameFilterPanel.add(nameFilterButton);
+        
+        incomeFilterInput            = new JTextField(20);
+        incomeFilterButton           = new JButton("Filter by Income Group");
+        incomeFilterPanel            = new JPanel(new GridLayout(1,2));
+        incomeFilterPanel.add(incomeFilterInput);
+        incomeFilterPanel.add(incomeFilterButton);
+
+        unemploymentFilterLower      = new JTextField(10);
+        unemploymentFilterUpper      = new JTextField(10);
+        unemploymentFilterButton     = new JButton("     Filter by Unemployment     ");
+        unemploymentFilterPanel      = new JPanel(new GridBagLayout());
+        unemploymentFilterPanel.add(unemploymentFilterLower);
+        unemploymentFilterPanel.add(unemploymentFilterUpper);
+        unemploymentFilterPanel.add(unemploymentFilterButton);
+
+        popFilterLower               = new JTextField(10);
+        popFilterUpper               = new JTextField(10);
+        popFilterButton              = new JButton("      Filter by Population      ");
+        popFilterPanel               = new JPanel(new GridBagLayout());
+        popFilterPanel.add(popFilterLower);
+        popFilterPanel.add(popFilterUpper);
+        popFilterPanel.add(popFilterButton);
+
+        unemploymentFilterRangeLower      = new JTextField(10);
+        unemploymentFilterRangeUpper      = new JTextField(10);
+        unemploymentFilterRangeButton     = new JButton("  Filter by Unemployment Range  ");
+        unemploymentFilterRangePanel      = new JPanel(new GridBagLayout());
+        unemploymentFilterRangePanel.add(unemploymentFilterRangeLower);
+        unemploymentFilterRangePanel.add(unemploymentFilterRangeUpper);
+        unemploymentFilterRangePanel.add(unemploymentFilterRangeButton);
+
+        popFilterRangeLower               = new JTextField(10);
+        popFilterRangeUpper               = new JTextField(10);
+        popFilterRangeButton              = new JButton("   Filter by Population Range   ");
+        popFilterRangePanel               = new JPanel(new GridBagLayout());
+        popFilterRangePanel.add(popFilterRangeLower);
+        popFilterRangePanel.add(popFilterRangeUpper);
+        popFilterRangePanel.add(popFilterRangeButton);
+
+        filterAll = new JButton(" Filter by All");
+        filterAll.setPreferredSize(new Dimension(900, 30));
+        
+        allFilterPanel = new JPanel();
+        allFilterPanel.add(filterAll);
+
+
+        resultsArea = new JTextArea(40, 100);
+        resultsArea.setFont(new Font("Monospaced", Font.PLAIN, 10));
         resultsArea.setEditable(false);
- 
+
+        statsArea = new JTextArea(8, 40);
+        statsArea.setFont(new Font("Monospaced", Font.PLAIN, 12));
+        statsArea.setEditable(false);
+
+        screenshotListButton  = new JButton("                  Screenshot Country List                  ");
+        screenshotStatsButton = new JButton("                   Screenshot Statistics                   ");
+
+        screenshotPanel = new JPanel(new GridLayout(2,1));
+        screenshotPanel.add(screenshotListButton);
+        screenshotPanel.add(screenshotStatsButton);
+
+        sidePanel = new JPanel(new GridLayout(2,1));
+        sidePanel.add(screenshotPanel);
+        sidePanel.add(statsArea);
+
+
         // Add components to window
-        add(inputField);
-        add(statusButton);
-        add(percentageButton);
+        add(nameFilterPanel);
+        add(incomeFilterPanel);
+        add(unemploymentFilterPanel);
+        add(popFilterPanel);
+        add(unemploymentFilterRangePanel);
+        add(popFilterRangePanel);
+        add(allFilterPanel);
+
         add(new JScrollPane(resultsArea));
- 
+        add(sidePanel);
+        
         // Setup button actions
-        statusButton.addActionListener(e -> analyzeStatus());
-        percentageButton.addActionListener(e -> showPercentage());
+        filterAll.addActionListener(e -> filterAll());
+        nameFilterButton.addActionListener(e -> {
+            analyzer.filterName(nameFilterInput.getText());
+            refresh();
+        });
+        incomeFilterButton.addActionListener(e -> {
+            analyzer.filterIncome(incomeFilterInput.getText());
+            refresh();
+        });
+        unemploymentFilterButton.addActionListener(e -> {
+            analyzer.filterUnemployment(unemploymentFilterLower.getText(), unemploymentFilterUpper.getText());
+            refresh();
+        });
+        popFilterButton.addActionListener(e -> {
+            analyzer.filterPopulation(popFilterLower.getText(), popFilterUpper.getText());
+            refresh();
+        });
+        unemploymentFilterRangeButton.addActionListener(e -> {
+            analyzer.filterUnemploymentRange(unemploymentFilterRangeLower.getText(), unemploymentFilterRangeUpper.getText());
+            refresh();
+        });
+        popFilterRangeButton.addActionListener(e -> {
+            analyzer.filterPopulationRange(popFilterRangeLower.getText(), popFilterRangeUpper.getText());
+            refresh();
+        });
+        screenshotListButton.addActionListener(e -> {
+            saveScreen(resultsArea);
+        });
+        screenshotStatsButton.addActionListener(e -> {
+            saveScreen(statsArea);
+        });
  
         setVisible(true);
     }
- 
-    private void analyzeStatus() {
-        String status = inputField.getText();
-        resultsArea.setText("Analyzing status: " + status);
-        analyzer.identifyHighRiskCommunities(status, 10);
+
+    public void refresh() {
+        resultsArea.setText(analyzer.getFilteredCountryTable());
+        statsArea.setText(analyzer.getFilteredCountryStats());
     }
- 
-    private void showPercentage() {
-        double average = analyzer.calculateAverageUnemployment();
-        resultsArea.setText("Average unemployment: " + average + "%");
+
+    public void filterAll() {
+        analyzer.filterAll(nameFilterInput.getText(), incomeFilterInput.getText(), unemploymentFilterLower.getText(), unemploymentFilterUpper.getText(), popFilterLower.getText(), popFilterUpper.getText(), unemploymentFilterRangeLower.getText(), unemploymentFilterRangeUpper.getText(), popFilterRangeLower.getText(), popFilterRangeUpper.getText());
+        refresh();
     }
- 
+    
+    public void saveScreen(JTextArea area) {
+        screenshot_counter++;
+        try {
+            // Create a BufferedImage to store content
+            int w = area.getWidth();
+            int h = area.getHeight();
+            int type = BufferedImage.TYPE_INT_ARGB;
+            BufferedImage sshot = new BufferedImage(w, h, type);
+
+            // Grab the canvas of the image
+            Graphics2D g2d = sshot.createGraphics();
+            // Like a virtual brush, paint the canvas with resultArea
+            area.paint(g2d);
+
+            // Make a file
+            File out = new File("Search" + screenshot_counter + ".png");
+            // Save the content of the image to this file
+            ImageIO.write(sshot, "png", out);
+
+            // Remove the canvas
+            g2d.dispose();
+
+            System.out.println("Screenshot saved: " + out.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        new DataGUI();
+        DataGUI y = new DataGUI();
+        y.filterAll();
     }
 }
